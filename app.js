5c9c3e51b1c4711443cff72e0d71400ab6314b31
unknown <marcinxmurach995@gmail.com>
2019-05-25 19:20:06 +0200

    port fix

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 8001;
 
 app.get('/', (req, res) => {
 	res.status(200).send({
@@ -15,5 +14,5 @@ app.get('/getData', (req, res) => {
   	});
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 8001, () => {
 });
