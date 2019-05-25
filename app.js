94e51ea568795587f692f85deb2082adfdde42ba
unknown <marcinxmurach995@gmail.com>
2019-05-25 16:41:00 +0200

    / message

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8001;
 
+app.get('/', (req, res) => {
+	res.status(200).send({
+		status: 'OK'
+  	});
+});
+
 app.get('/getData', (req, res) => {
 	res.status(200).send({
 		dane: req.query.variables,
